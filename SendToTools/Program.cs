8424515a31497f416b71b049462c8aab1f866088
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2010-04-11 01:15:40 +0000

    start
    git-svn-id: svn://192.168.0.99/SendToTools@2079 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ SendToTools/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ShortcutRenamer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                MessageBox.Show("引数がありません",
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+                return;
+            }
+
+            // string shortcutfile = @"C:\Documents and Settings\xpenpen\SendTo\VCExpress.exe へのショートカット.lnk";
+            string shortcutfile = args[0];
+
+            if (!System.IO.File.Exists(shortcutfile))
+            {
+                MessageBox.Show("ファイル " + shortcutfile + " は存在しません",
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+                return;
+            }
+
+            try
+            {
+                System.IO.FileInfo fi = new System.IO.FileInfo(shortcutfile);
+                string dir = fi.Directory.FullName;
+                string oldname = fi.Name;
+
+                string newname = oldname.Replace(".exe へのショートカット", "");
+
+                fi.MoveTo(dir + @"\" + newname);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message,
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+            }
+        }
+    }
+}
