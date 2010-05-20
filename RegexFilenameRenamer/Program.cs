83e7cf7a8b844b2e1a5bfd5fab70232a8c991ae5
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2010-05-20 03:02:02 +0000

    iii
    git-svn-id: svn://192.168.0.99/SendToTools@2269 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ RegexFilenameRenamer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RegexFilenameRenamer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+        }
+    }
+}
