1c052ec6b8f604dc5a9c564d3ed631edf81048e5
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2010-05-25 07:52:58 +0000

    iii
    git-svn-id: svn://192.168.0.99/SendToTools@2296 5ada3c48-002d-0410-850d-99865cc6fd0f

--- RegexFilenameRenamer/Program.cs
+++ RegexFilenameRenamer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace Ambiesoft.RegexFilenameRenamer
 {
@@ -13,29 +15,49 @@ namespace Ambiesoft.RegexFilenameRenamer
         static int Main(string[] args)
         {
             SimpleCommandLineParser parser = new SimpleCommandLineParser(args);
-            parser.addOption("reg", ARGUMENT_TYPE.MUST);
-            parser.addOption("at", ARGUMENT_TYPE.MUST);
-            parser.addOption("ah", ARGUMENT_TYPE.MUST);
-            parser.addOption("sb", ARGUMENT_TYPE.MUST);
-            parser.addOption("sa", ARGUMENT_TYPE.MUST);
-            parser.addOption("sr", ARGUMENT_TYPE.MUST);
+            parser.addOption("rf", ARGUMENT_TYPE.MUST);
+            parser.addOption("rt", ARGUMENT_TYPE.MUST);
+            parser.addOption("ie", ARGUMENT_TYPE.MUSTNOT);
             parser.Parse();
 
-            if (!parser["sr"])
+            if (parser["rf"] == null || parser["rt"] == null)
             {
+                MessageBox.Show("-rf と -rtは両方指定です。");
                 return -1;
             }
 
-            string sr = parser["sr"].ToString();
+            string f = parser["rf"].ToString();
+            string t = parser["rt"].ToString();
 
             if (parser.MainargLength != 1)
             {
+                MessageBox.Show("ファイルが指定されていません。");
                 return -1;
             }
 
-            string orgFileName = parser.get_Mainargs[0];
+            try
+            {
+                Regex regf = new Regex(f);
+                string orgFullorRelativeFileName = parser.get_Mainargs(0);
 
+                bool isAlsoExt = null != parser["ie"];
 
+                FileInfo fiorig = new FileInfo(orgFullorRelativeFileName);
+                string orgFileName = fiorig.Name;
+                string orgFolder = fiorig.DirectoryName;
+
+                string newFileName = regf.Replace(orgFileName, t);
+                fiorig.MoveTo(orgFolder + @"\" + newFileName);
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message,Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return -1;
+            }
         }
     }
 }
