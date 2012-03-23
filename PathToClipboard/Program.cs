10459841b8dea98ee2b8aeb52b54f8c7ddddc849
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2012-03-23 09:34:40 +0000

    ii
    git-svn-id: svn://192.168.0.99/SendToTools@3756 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ PathToClipboard/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PathToClipboard
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                MessageBox.Show("引数がありません",
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+                return;
+            }
+
+            try
+            {
+                Clipboard.SetText(args[0]);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
