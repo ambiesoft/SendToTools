2d589f4d5ac9870e5ceff6b8586f46f0427870d4
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2014-05-07 07:09:39 +0000

    run as file
    git-svn-id: svn://192.168.0.99/SendToTools@4784 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ RunasFile/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace RunasFile
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string theFileName;
+            if (args.Length < 1)
+            {
+                MessageBox.Show("No arguments",
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+                return;
+            }
+            if (args.Length == 1)
+            {
+                theFileName = args[0];
+                if (!System.IO.File.Exists(theFileName))
+                {
+                    MessageBox.Show(theFileName + " not exists",
+                        Application.ProductName,
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Asterisk);
+                    return;
+                }
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = Application.ExecutablePath;
+                startInfo.UseShellExecute = true;
+                startInfo.Verb = "runas";
+                startInfo.Arguments = "/run \"" + theFileName + "\"";
+                
+
+                try
+                {
+                    Process proc = Process.Start(startInfo);
+                    // proc.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                return;
+
+            }
+            else if (args.Length == 2)
+            {
+                if (args[0] != "/run")
+                {
+                    MessageBox.Show("args[0] is not /run",
+                        Application.ProductName,
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Asterisk);
+                    return;
+                }
+                theFileName = args[1];
+
+
+
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = theFileName;
+                startInfo.UseShellExecute = true;
+                startInfo.Verb = "open";
+                //startInfo.Arguments = "start \"\" \"" + theFileName + "\"";
+                //startInfo.Arguments = theFileName;
+
+                try
+                {
+                    Process proc = Process.Start(startInfo);
+                    // proc.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Too many arguments",
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+                return;
+
+            }
+        }
+    }
+}
