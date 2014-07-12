ec13539080b6d80bf5de1c698dd203c333687a21
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2014-07-12 15:50:24 +0000

    iii
    git-svn-id: svn://192.168.0.99/SendToTools@4923 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ argCheck/argCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace argCheck
+{
+    static class argCheck
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine("total length = " + System.Environment.CommandLine.Length);
+            sb.AppendLine("arg count = " + args.Length);
+
+            foreach (string item in args)
+            {
+                sb.AppendLine(item);
+            }
+
+
+            MessageBox.Show(sb.ToString(),
+                Application.ProductName,
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
