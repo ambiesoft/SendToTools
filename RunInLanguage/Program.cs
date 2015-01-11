0bccd7a7e2985b527efbbb3a5b4b7532055285d5
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-01-11 08:23:00 +0000

    ii
    git-svn-id: svn://192.168.0.99/SendToTools@5101 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ RunInLanguage/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace RunInLanguage
+{
+    static class Program
+    {
+        static void dowork(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                MessageBox.Show(Properties.Resources.ArgNotExist);
+                return;
+            }
+
+            string prog = "";
+            string progarg = "";
+            for (int i = 0; i < args.Length; ++i)
+            {
+                string t;
+                if (args[i].IndexOf(' ') >= 0)
+                {
+                    t = "\"" + args[i] + "\"";
+                }
+                else
+                {
+                    t = args[i];
+                }
+
+                if (i == 0)
+                {
+                    prog = t;
+                }
+                else
+                {
+                    progarg += t;
+                }
+            }
+
+            //string cmdline = System.Environment.CommandLine;
+            //cmdline=cmdline.TrimStart(' ');
+            //if(cmdline[0]=='\"')
+            //{
+            //    int i = cmdline.IndexOf('\"',  1);
+            //    if(i<0)
+            //    {
+            //        throw new Exception("Unknown Error");
+            //    }
+            //    cmdline = cmdline.Substring(i+1);
+            //    cmdline = cmdline.TrimStart();
+            //}
+            //else if(cmdline[0]==' ')
+            //{
+            //    int i = cmdline.IndexOf(' ',  1);
+            //    if(i<0)
+            //    {
+            //        throw new Exception("Unknown Error");
+            //    }
+            //    cmdline = cmdline.Substring(i+1);
+            //    cmdline = cmdline.TrimStart();
+            //}
+
+
+            System.Environment.SetEnvironmentVariable("__COMPAT_LAYER", "#APPLICATIONLOCALE");
+            System.Environment.SetEnvironmentVariable("AppLocaleID", "1252");
+            //System.Environment.SetEnvironmentVariable("AppLocaleID", "0411");
+
+            Process.Start(prog, progarg);
+        }
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                dowork(args);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+       
+    }
+}
