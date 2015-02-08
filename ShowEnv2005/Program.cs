bcc4282b4ed2cf4b36d6f82c533d40b2cc95d117
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-02-08 06:07:58 +0000

    iii
    git-svn-id: svn://192.168.0.99/SendToTools@5251 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ ShowEnv2005/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace ShowEnv2005
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            List<string> result = new List<string>();
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+            {
+                string key = (string)de.Key;
+                string value = (string)de.Value;
+
+                result.Add(key + "=" + value);
+            }
+            result.Sort();
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (string s in result)
+            {
+                sb.AppendLine(s);
+            }
+            MessageBox.Show(
+                sb.ToString(),
+                Application.ProductName,
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
