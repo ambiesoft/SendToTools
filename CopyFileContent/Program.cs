8520e1ae5b59bc9d5778748cda68a8b2ea2ad858
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-02-14 07:29:10 +0000

    iii
    git-svn-id: svn://192.168.0.99/SendToTools@5283 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ CopyFileContent/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CopyFileContent
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                MessageBox.Show(Properties.Resources.NO_ARG,
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+                return;
+            }
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(args[0]);
+                // Clipboard.SetText(lines[0],TextDataFormat.);
+
+                NotifyIcon ni = new NotifyIcon();
+                ni.BalloonTipTitle = "btttt";
+                ni.BalloonTipText = Properties.Resources.CLIPBOARDSET;
+                ni.Icon = Properties.Resources.icon;
+
+                ni.Text = Application.ProductName;
+                ni.Visible = true;
+                ni.ShowBalloonTip(5*1000);
+
+                System.Threading.Thread.Sleep(5 * 1000);
+                ni.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
