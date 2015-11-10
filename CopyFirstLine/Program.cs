5572c9afabd9af85e29203668b3bf77259f54a98
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-11-10 08:21:23 +0000

    ii
    git-svn-id: svn://192.168.0.99/SendToTools@5686 5ada3c48-002d-0410-850d-99865cc6fd0f

--- CopyFirstLine/Program.cs
+++ CopyFirstLine/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 
-namespace CopyFirstFile
+namespace CopyFirstLine
 {
     static class Program
     {
--- CopyFirstLine/Properties/Resources.Designer.cs
+++ CopyFirstLine/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CopyFirstFile.Properties {
+namespace CopyFirstLine.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CopyFirstFile.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CopyFirstFile.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CopyFirstLine.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
