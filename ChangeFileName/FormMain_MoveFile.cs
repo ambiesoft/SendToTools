95a4af7b6dcc33985a7e9459913cbc696e18e261
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2015-12-08 17:30:50 +0000

    tmp
    git-svn-id: svn://192.168.0.99/SendToTools@5904 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ ChangeFileName/FormMain_MoveFile.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChangeFileName
+{
+    public partial class FormMain : Form
+    {
+        private void moveToAndClose(string path)
+        {
+            try
+            {
+                System.IO.FileInfo fiorig = new System.IO.FileInfo(this.textName.Tag.ToString());
+                string destfilename = textName.Text + fiorig.Extension;
+                string destfn = System.IO.Path.Combine(path, destfilename);
+                fiorig.MoveTo(destfn);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                showError(ex.Message);
+            }
+        }
+
+        private void itemNewFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            using (new CenterWinDialog(this))
+            {
+                if (DialogResult.OK != fbd.ShowDialog(this))
+                    return;
+            }
+            List<string> dirs = new List<string>(Dirs);
+            dirs.Insert(0, fbd.SelectedPath);
+            Dirs = dirs.ToArray();
+
+            moveToAndClose(fbd.SelectedPath);
+        }
+        private void itemExistingFolder_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            if(item==null)
+            {
+                return;
+            }
+            string dir = item.Tag as string;
+            if(dir==null || !System.IO.Directory.Exists(dir))
+            {
+                // LANG
+                showError(dir + " is not a directory");
+                return;
+            }
+            moveToAndClose(dir);
+        }
+
+        private string[] Dirs
+        {
+            get
+            {
+                string[] dirs;
+                Ambiesoft.Profile.GetStringArray("settings", "movetodirs", out dirs, IniFile);
+                return dirs;
+            }
+            set
+            {
+                if(!Ambiesoft.Profile.WriteStringArray("settings", "movetodirs", value, IniFile))
+                {
+                    // LANG
+                    showError("save failed");
+                }
+            }
+        }
+        private void btnMoveTo_Click(object sender, EventArgs e)
+        {
+            menuMoveTo.Items.Clear();
+
+            foreach (string dir in Dirs)
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem();
+                item.Click += new EventHandler(itemExistingFolder_Click);
+                item.Text = dir; // System.IO.Path.GetDirectoryName(dir);
+                item.Tag = dir;
+                menuMoveTo.Items.Add(item);
+            }
+
+            menuMoveTo.Items.Add(new ToolStripSeparator());
+
+            ToolStripMenuItem itemNewFolder = new ToolStripMenuItem();
+            itemNewFolder.Click += new EventHandler(itemNewFolder_Click);
+            itemNewFolder.Text = "&New Folder...";
+            menuMoveTo.Items.Add(itemNewFolder);
+
+            Point pt = btnMoveTo.Location;
+            pt.Y += btnMoveTo.Size.Height;
+            menuMoveTo.Show(this.PointToScreen(pt));
+        }
+
+
+
+    }
+}
+
