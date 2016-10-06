ea5164c8761a69da408e4f73164d45a7ff9df71b
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2016-10-06 08:43:17 +0000

    iii
    git-svn-id: svn://192.168.0.99/SendToTools@6217 5ada3c48-002d-0410-850d-99865cc6fd0f

--- PathToClipboard/PathToClipboard.cpp
+++ PathToClipboard/PathToClipboard.cpp
@@ -7,15 +7,33 @@
 #include "../../MyUtility/SetClipboardText.h"
 #include "../../MyUtility/showballoon.h"
 #include "../../MyUtility/stdwin32/stdwin32.h"
+#include "../../MyUtility/StdStringReplace.h"
+#include "../../MyUtility/CenterWindow.h"
+
 using namespace stdwin32;
 using namespace std;
 
 #define I18N(s) (s)
 
 #define KAIGYO L"\r\n"
+#define SPACE L" "
 
 #define MAX_LOADSTRING 100
 
+enum ConvertType {
+	CT_NORMAL,
+	CT_DOUBLESBACKLASH,
+	CT_SLASH,
+};
+
+struct DialogData {
+	ConvertType ct_;
+	bool dq_;
+	bool kaigyo_;
+	DialogData() {
+		ZeroMemory(this, sizeof(*this));
+	}
+};
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
@@ -31,6 +49,85 @@ void showErrorAndExit(LPCTSTR pMessage)
 	exit(1);
 }
 
+INT_PTR CALLBACK DialogProc(
+	_In_ HWND   hwndDlg,
+	_In_ UINT   uMsg,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+)
+{
+	static DialogData* sDT = NULL;
+	switch (uMsg)
+	{
+		case WM_INITDIALOG:
+		{
+			sDT = (DialogData*)lParam;
+			SendDlgItemMessage(hwndDlg, IDC_RADIO_NORMAL, BM_SETCHECK, BST_CHECKED, 0);
+			CenterWindow(hwndDlg);
+		}
+		break;
+
+		case WM_COMMAND:
+		{
+			switch (LOWORD(wParam))
+			{
+				case IDOK:
+				{
+					if (SendDlgItemMessage(hwndDlg, IDC_RADIO_NORMAL, BM_GETCHECK, 0, 0))
+						sDT->ct_ = CT_NORMAL;
+					else if (SendDlgItemMessage(hwndDlg, IDC_RADIO_TWOBACKSLASH, BM_GETCHECK, 0, 0))
+						sDT->ct_ = CT_DOUBLESBACKLASH;
+					else if (SendDlgItemMessage(hwndDlg, IDC_RADIO_SLASH, BM_GETCHECK, 0, 0))
+						sDT->ct_ = CT_SLASH;
+
+					sDT->dq_ = !!SendDlgItemMessage(hwndDlg, IDC_CHECK_DQ, BM_GETCHECK, 0, 0);
+					sDT->kaigyo_ = !!SendDlgItemMessage(hwndDlg, IDC_CHECK_LINE, BM_GETCHECK, 0, 0);
+					
+					
+					EndDialog(hwndDlg, IDOK);
+					return TRUE;
+				}
+				break;
+			
+				case IDCANCEL:
+				{
+					EndDialog(hwndDlg, IDCANCEL);
+					return TRUE;
+				}
+			}
+			break;
+		}
+		break;
+	}
+	return FALSE;
+}
+
+tstring ConvertPath(const DialogData& dt, LPCTSTR pPath)
+{
+	tstring ret(pPath);
+
+	switch (dt.ct_)
+	{
+	case CT_NORMAL:
+		break;
+
+	case CT_DOUBLESBACKLASH:
+		ret = StdStringReplace(ret, _T("\\"), _T("\\\\"));
+		break;
+
+	case CT_SLASH:
+		ret = StdStringReplace(ret, _T("\\"), _T("/"));
+		break;
+	}
+
+	if (dt.dq_)
+	{
+		ret = _T("\"") + ret;
+		ret += _T("\"");
+	}
+
+	return ret;
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -40,7 +137,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	hInst = hInstance;
 
     // Initialize global strings
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -51,13 +148,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		showErrorAndExit(I18N(L"No Arguments"));
 	}
 
+	DialogData dt;
+	if (GetAsyncKeyState(VK_CONTROL) < 0)
+	{
+		if (IDOK != DialogBoxParam(hInst,
+			MAKEINTRESOURCE(IDD_PATHTOCLIPBOARD_DIALOG),
+			NULL,
+			DialogProc,
+			(LPARAM)&dt))
+		{
+			return 1;
+		}
+	}
+
 	wstring str;
 	for (int i = 1; i < __argc;++i)
 	{
-		str += __wargv[i];
-		str += KAIGYO;
+		str += ConvertPath(dt, __wargv[i]);
+		str += dt.kaigyo_ ? KAIGYO : SPACE;
 	}
-	str=trim(str, KAIGYO);
+	str=trim(str, dt.kaigyo_ ? KAIGYO : SPACE);
 
 	if (!SetClipboardText(NULL, str.c_str()))
 	{
