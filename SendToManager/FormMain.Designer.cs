f2cabb5876e23b1d8b4eeef3619d804eba239194
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-08-11 09:04:43 +0000

    git-svn-id: svn://192.168.0.99/SendToTools@7354 5ada3c48-002d-0410-850d-99865cc6fd0f

--- SendToManager/FormMain.Designer.cs
+++ SendToManager/FormMain.Designer.cs
@@ -30,34 +30,105 @@
         {
             this.lvMain = new System.Windows.Forms.ListView();
             this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pgItem = new System.Windows.Forms.PropertyGrid();
+            this.spMain = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addNewItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.spMain)).BeginInit();
+            this.spMain.Panel1.SuspendLayout();
+            this.spMain.Panel2.SuspendLayout();
+            this.spMain.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lvMain
             // 
             this.lvMain.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chNo,
             this.chName});
-            this.lvMain.Location = new System.Drawing.Point(12, 12);
+            this.lvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvMain.FullRowSelect = true;
+            this.lvMain.Location = new System.Drawing.Point(0, 0);
             this.lvMain.Name = "lvMain";
-            this.lvMain.Size = new System.Drawing.Size(334, 515);
+            this.lvMain.Size = new System.Drawing.Size(263, 457);
             this.lvMain.TabIndex = 0;
             this.lvMain.UseCompatibleStateImageBehavior = false;
             this.lvMain.View = System.Windows.Forms.View.Details;
+            this.lvMain.VirtualMode = true;
+            this.lvMain.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.lvMain_RetrieveVirtualItem);
+            this.lvMain.VirtualItemsSelectionRangeChanged += new System.Windows.Forms.ListViewVirtualItemsSelectionRangeChangedEventHandler(this.lvMain_VirtualItemsSelectionRangeChanged);
             // 
             // chName
             // 
             this.chName.Text = "Name";
-            this.chName.Width = 237;
+            this.chName.Width = 300;
+            // 
+            // chNo
+            // 
+            this.chNo.Text = "No.";
+            this.chNo.Width = 24;
+            // 
+            // pgItem
+            // 
+            this.pgItem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pgItem.LineColor = System.Drawing.SystemColors.ControlDark;
+            this.pgItem.Location = new System.Drawing.Point(0, 0);
+            this.pgItem.Name = "pgItem";
+            this.pgItem.Size = new System.Drawing.Size(522, 457);
+            this.pgItem.TabIndex = 4;
+            // 
+            // spMain
+            // 
+            this.spMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spMain.Location = new System.Drawing.Point(0, 24);
+            this.spMain.Name = "spMain";
+            // 
+            // spMain.Panel1
+            // 
+            this.spMain.Panel1.Controls.Add(this.lvMain);
+            // 
+            // spMain.Panel2
+            // 
+            this.spMain.Panel2.Controls.Add(this.pgItem);
+            this.spMain.Size = new System.Drawing.Size(789, 457);
+            this.spMain.SplitterDistance = 263;
+            this.spMain.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewItemToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(789, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addNewItemToolStripMenuItem
+            // 
+            this.addNewItemToolStripMenuItem.Name = "addNewItemToolStripMenuItem";
+            this.addNewItemToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.addNewItemToolStripMenuItem.Text = "Add &New Item";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 539);
-            this.Controls.Add(this.lvMain);
+            this.ClientSize = new System.Drawing.Size(789, 481);
+            this.Controls.Add(this.spMain);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "FormMain";
-            this.Text = "Form1";
+            this.Text = "SendTo Manager";
             this.Load += new System.EventHandler(this.FormMain_Load);
+            this.spMain.Panel1.ResumeLayout(false);
+            this.spMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spMain)).EndInit();
+            this.spMain.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -65,6 +136,11 @@
 
         private System.Windows.Forms.ListView lvMain;
         private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chNo;
+        private System.Windows.Forms.PropertyGrid pgItem;
+        private System.Windows.Forms.SplitContainer spMain;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addNewItemToolStripMenuItem;
     }
 }
 
--- SendToManager/FormMain.cs
+++ SendToManager/FormMain.cs
@@ -16,11 +16,13 @@ namespace SendToManager
             InitializeComponent();
         }
 
+        SortedList<int, FileInfo> allitems = new SortedList<int, FileInfo>();
+
         private void FormMain_Load(object sender, EventArgs e)
         {
             string dir = System.Environment.GetFolderPath(Environment.SpecialFolder.SendTo);
             System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(dir);
-            System.IO.FileInfo[] fis = di.GetFiles("*.lnk", SearchOption.TopDirectoryOnly);
+            System.IO.FileInfo[] fis = di.GetFiles("*.*", SearchOption.TopDirectoryOnly);
             Array.Sort(fis,
                 delegate(FileInfo f1, FileInfo f2)
                 {
@@ -28,15 +30,36 @@ namespace SendToManager
                 }
             );
 
+            int i = 0;
             foreach (FileInfo fi in fis)
             {
                 if (!fi.IsReadOnly &&
                     (fi.Attributes & FileAttributes.Hidden)==0 &&
                     string.Compare(fi.Extension, ".lnk", true)==0 )
                 {
-                    lvMain.Items.Add(Path.GetFileNameWithoutExtension(fi.Name));
+                    allitems.Add(i++, fi);
                 }
             }
+
+            lvMain.VirtualListSize = allitems.Count;
+        }
+
+        private void lvMain_VirtualItemsSelectionRangeChanged(object sender, ListViewVirtualItemsSelectionRangeChangedEventArgs e)
+        {
+            
+        }
+
+        private void lvMain_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            
+            FileInfo fi = allitems[e.ItemIndex];
+
+            var item = new ListViewItem();
+            item.Text = string.Format("{0:D2}", e.ItemIndex+1);
+            item.SubItems.Add(fi.Name);
+
+
+            e.Item = item;
         }
     }
 }
