96a1dea7177a5c67150a2ef6cd85f4c13fc7bef5
ambiesoft <ambiesoft.trueff@gmail.com>
2019-06-16 21:25:53 +0900

    add CreateSizedFile

---
+++ CreateSizedFile/FormMain.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CreateSizedFile
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.lblFilename = new System.Windows.Forms.Label();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.chkRandom = new System.Windows.Forms.CheckBox();
+            this.chkZero = new System.Windows.Forms.CheckBox();
+            this.btnNavigate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFilename
+            // 
+            resources.ApplyResources(this.lblFilename, "lblFilename");
+            this.lblFilename.Name = "lblFilename";
+            // 
+            // txtFilename
+            // 
+            resources.ApplyResources(this.txtFilename, "txtFilename");
+            this.txtFilename.Name = "txtFilename";
+            // 
+            // lblSize
+            // 
+            resources.ApplyResources(this.lblSize, "lblSize");
+            this.lblSize.Name = "lblSize";
+            // 
+            // txtSize
+            // 
+            resources.ApplyResources(this.txtSize, "txtSize");
+            this.txtSize.Name = "txtSize";
+            // 
+            // btnOK
+            // 
+            resources.ApplyResources(this.btnOK, "btnOK");
+            this.btnOK.Name = "btnOK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            resources.ApplyResources(this.btnCancel, "btnCancel");
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // chkRandom
+            // 
+            resources.ApplyResources(this.chkRandom, "chkRandom");
+            this.chkRandom.Name = "chkRandom";
+            this.chkRandom.UseVisualStyleBackColor = true;
+            this.chkRandom.CheckedChanged += new System.EventHandler(this.chkRandom_CheckedChanged);
+            // 
+            // chkZero
+            // 
+            resources.ApplyResources(this.chkZero, "chkZero");
+            this.chkZero.Name = "chkZero";
+            this.chkZero.UseVisualStyleBackColor = true;
+            this.chkZero.CheckedChanged += new System.EventHandler(this.chkZero_CheckedChanged);
+            // 
+            // btnNavigate
+            // 
+            resources.ApplyResources(this.btnNavigate, "btnNavigate");
+            this.btnNavigate.Name = "btnNavigate";
+            this.btnNavigate.UseVisualStyleBackColor = true;
+            this.btnNavigate.Click += new System.EventHandler(this.btnNavigate_Click);
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.btnOK;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.Controls.Add(this.btnNavigate);
+            this.Controls.Add(this.chkZero);
+            this.Controls.Add(this.chkRandom);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.lblFilename);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFilename;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        internal System.Windows.Forms.CheckBox chkRandom;
+        internal System.Windows.Forms.CheckBox chkZero;
+        private System.Windows.Forms.Button btnNavigate;
+    }
+}
---
+++ CreateSizedFile/FormMain.cs
@@ -0,0 +1,160 @@
+//BSD 2-Clause License
+//
+//Copyright (c) 2017, Ambiesoft
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met:
+//
+//* Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//
+//* Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Ambiesoft;
+
+namespace CreateSizedFile
+{
+    public partial class FormMain : Form
+    {
+        readonly string initialFile_;
+        public FormMain(string initialFile)
+        {
+            initialFile_ = initialFile;
+            InitializeComponent();
+
+            txtFilename.Text = initialFile;
+        }
+
+         private void chkRandom_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkRandom.Checked && chkZero.Checked)
+            {
+                chkZero.Checked = false;
+            }
+        }
+
+        private void chkZero_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkRandom.Checked && chkZero.Checked)
+            {
+                chkRandom.Checked = false;
+            }
+        }
+
+        Int64 GetCreatingFileSize()
+        {
+            Int64 ret;
+            if (!Int64.TryParse(txtSize.Text, out ret))
+                return -1;
+            return ret;
+        }
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Int64 fsize = GetCreatingFileSize();
+            if (fsize < 0)
+            {
+                MessageBox.Show(Properties.Resources.FILESIZE_NOTBE_MINUS,
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+                return;
+            }
+            if (fsize > ((Int64)2 * 1024 * 1024 * 1024))
+            {
+                if (DialogResult.Yes != MessageBox.Show(
+                    string.Format(Properties.Resources.ARE_YOU_GOING_TO_CREATE, fsize),
+                    Application.ProductName,
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question))
+                {
+                    return;
+                }
+            }
+            try
+            {
+
+                if (File.Exists(txtFilename.Text))
+                {
+                    if (DialogResult.Yes != MessageBox.Show(
+                        string.Format(Properties.Resources.ALREADY_EXISTS_DO_YOU_WANT_TO_OVERRIDE, txtFilename.Text),
+                        Application.ProductName,
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question))
+                    {
+                        return;
+                    }
+                }
+                FileStream fs = File.Create(
+                    txtFilename.Text, 
+                    1,
+                    System.IO.FileOptions.RandomAccess);
+                if (chkRandom.Checked)
+                {
+                    System.Random r = new Random();
+                    for (long i = 0; i < fsize; ++i)
+                    {
+                        byte[] b = new byte[1];
+                        r.NextBytes(b);
+                        fs.WriteByte(b[0]);
+                    }
+                }
+                else if (chkZero.Checked)
+                {
+                    for (long i = 0; i < fsize; ++i)
+                    {
+                        fs.WriteByte(0);
+                    }
+                }
+                else
+                {
+                    fs.SetLength(fsize);
+                }
+                fs.Close();
+                CppUtils.CenteredMessageBox(Properties.Resources.FILE_CREATED,
+                    Application.ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,
+                    ProductName,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNavigate_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                if (DialogResult.OK != dlg.ShowDialog())
+                    return;
+
+                txtFilename.Text = dlg.FileName;
+            }            
+        }
+    }
+}
