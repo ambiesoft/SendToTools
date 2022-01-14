3408882cbde4870f990c0bf874c56c10e9ecc3c9
ambiesoft <ambiesoft.trueff@gmail.com>
2022-01-14 14:01:17 +0900

    Add CommonControlv6 support on CopyPath and SwapFilename

--- SwapFilename/StdAfx.cpp
+++ SwapFilename/StdAfx.cpp
@@ -1,3 +1,7 @@
 #include "StdAfx.h"
 
 #pragma comment(lib, "shlwapi.lib")
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
