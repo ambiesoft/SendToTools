ca069b8f79cf8f0a699867b850e72b585aab851b
ambiesoft <ambiesoft.trueff@gmail.com>
2023-10-22 21:39:15 +0900

    SwapFilename v2.0.3 Added '-autodetectall' and '-removeoldif'

--- SwapFilename/main.cpp
+++ SwapFilename/main.cpp
@@ -106,7 +106,12 @@ BOOL DoReplaceFileWithReplaceFile(wstring file1, wstring file2, wstring fileback
 	return TRUE;
 }
 
-static wstring GetOlderFile(const wstring& file1, const wstring& file2, bool* pIsSame)
+struct OldAndNew
+{
+	wstring olderFile;
+	wstring newerFile;
+};
+static OldAndNew GetOlderFile(const wstring& file1, const wstring& file2, bool* pIsSame)
 {
 	CFileHandle h1(CreateFile(file1.c_str(),
 		GENERIC_READ,
@@ -123,17 +128,27 @@ static wstring GetOlderFile(const wstring& file1, const wstring& file2, bool* pI
 		FILE_ATTRIBUTE_NORMAL,
 		NULL));
 	if (!h1 || !h2)
-		return wstring();
+		return OldAndNew();
 
-	FILETIME ftCreation1, ftCreation2, ftLastWrite1, ftLastWrite2;
-	if (!GetFileTime(h1, &ftCreation1, NULL, &ftLastWrite1) || !GetFileTime(h2, &ftCreation2, NULL, &ftLastWrite2))
-		return wstring();
-	FILETIME ft1 = CompareFileTime(&ftCreation1, &ftLastWrite1) > 0 ? ftCreation1 : ftLastWrite1;
-	FILETIME ft2 = CompareFileTime(&ftCreation2, &ftLastWrite2) > 0 ? ftCreation2 : ftLastWrite2;
+	FILETIME ftLastWrite1, ftLastWrite2;
+	if (!GetFileTime(h1, NULL, NULL, &ftLastWrite1) || !GetFileTime(h2, NULL, NULL, &ftLastWrite2))
+		return OldAndNew();
 
-	LONG result = CompareFileTime(&ft1, &ft2);
+	LONG result = CompareFileTime(&ftLastWrite1, &ftLastWrite2);
 	*pIsSame = (result == 0);
-	return result < 0 ? file1 : file2;
+
+	OldAndNew ret;
+	if (result < 0) 
+	{
+		ret.olderFile = file1;
+		ret.newerFile = file2;
+	} 
+	else
+	{
+		ret.olderFile = file2;
+		ret.newerFile = file1;
+	}
+	return ret;
 }
 
 static bool showballoonexe(
@@ -187,120 +202,13 @@ bool GetFileSizeFromPath(const wchar_t* pPath, LARGE_INTEGER* pLI)
 		return false;
 	return !!GetFileSizeEx(h1, pLI);
 }
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow)
+
+
+bool doWork(wstring file1, wstring file2,
+	const bool bAlwaysYes, const bool bRemoveOld,
+	wstring* pAdditionalMessage,
+	function<bool(wstring oldFile, wstring newFile)> fnCondition)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
-	Ambiesoft::InitHighDPISupport();
-	wstring lang = GetLangFromCommandLine();
-	i18nInitLangmap(NULL, lang.c_str(), APPNAME);
-
-	CCommandLineParser parser(I18N(L"Swap filename of two files"));
-
-	bool bRemoveOld = false;
-	parser.AddOption(L"-removeold", 0, &bRemoveOld, 
-		ArgEncodingFlags::ArgEncodingFlags_Default,
-		I18N(L"Remove an older file after swapping"));
-
-	wstring strAutoDetect;
-	parser.AddOption(L"-autodetect", 1, &strAutoDetect,
-		ArgEncodingFlags::ArgEncodingFlags_Default,
-		I18N(L"Detects files automatically, files with same name but extensions will be detected"));
-
-	bool bAlwaysYes = false;
-	parser.AddOption(L"-alwaysyes", 0, &bAlwaysYes,
-		ArgEncodingFlags::ArgEncodingFlags_Default,
-		I18N(L"Answer 'yes' in all questions"));
-
-	bool bHelp = false;
-	parser.AddOptionRange({ L"-h",L"--help", L"/?" },
-		0,
-		&bHelp,
-		ArgEncodingFlags::ArgEncodingFlags_Default,
-		I18N(L"Show Help"));
-
-	bool bShowVersion = false;
-	parser.AddOptionRange({ L"-v",L"--version" },
-		0,
-		&bShowVersion,
-		ArgEncodingFlags::ArgEncodingFlags_Default,
-		I18N(L"Show Version"));
-
-	COption opMain(L"", 
-		ExactCount::Exact_2, 
-		ArgEncodingFlags_Default, 
-		I18N(L"Input two files"));
-	parser.AddOption(&opMain);
-
-	// lang is already defined
-	parser.AddOption(L"-lang",
-		1,
-		&lang,
-		ArgEncodingFlags_Default,
-		I18N(L"Language in 3-letter"));
-
-	parser.Parse();
-
-	if (parser.hadUnknownOption())
-	{
-		ErrorExit(I18N(L"Unknown Option:") + (L"\r\n" + parser.getUnknowOptionStrings()));
-	}
-	if (bShowVersion || bHelp)
-	{
-		MessageBox(NULL,
-			parser.getHelpMessage().c_str(),
-			stdFormat(L"%s v%s", APPNAME, GetVersionString(NULL, 3).c_str()).c_str(), 
-			MB_ICONINFORMATION);
-		return 0;
-	}
-
-	wstring file1;
-	wstring file2;
-
-	if(!strAutoDetect.empty())
-	{
-		// detect files
-		vector<wstring> files = stdGetFiles(strAutoDetect,
-			FILEITERATEMODE::SKIP_DOT_AND_DOTDOT,
-			GETFILESEMODE::FILE);
-
-		map<wstring, vector<wstring>> nameToFileNames;
-		for (auto&& file : files)
-		{
-			nameToFileNames[stdStringLower(stdGetFileNameWithoutExtension(file))].push_back(file);
-		}
-
-		for (auto&& kv : nameToFileNames)
-		{
-			if(nameToFileNames[kv.first].size()==2)
-			{
-				file1 = nameToFileNames[kv.first][0];
-				file2 = nameToFileNames[kv.first][1];
-				break;
-			}
-		}
-
-		if (file1.empty() || file2.empty())
-		{
-			ErrorExit(I18N(L"Files not found, Autodetects failed"));
-		}
-	}
-	else
-	{
-		if (opMain.getValueCount() != 2)
-		{
-			ErrorExit(stdFormat(
-				I18N(L"The count of files provided by command line is %d. Two files must be provided."),
-				opMain.getValueCount()));
-		}
-		file1 = opMain.getValue(0);
-		file2 = opMain.getValue(1);
-	}
-	
 	wstring fileback = GetBackupFile(file1.c_str());
 
 	constexpr const wchar_t* STR_FILE_DOES_NOT_EXIST = L"'%s' does not exist.";
@@ -319,12 +227,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	file1 = stdGetFullPathName(file1);
 	file2 = stdGetFullPathName(file2);
-	
+
 	FILETIME ftCreation1, ftLastAccess1, ftLastWrite1;
 	FILETIME ftCreation2, ftLastAccess2, ftLastWrite2;
 	bool bFtGot = false;
 	do
-	{	
+	{
 		CFileHandle h1(CreateFile(file1.c_str(),
 			GENERIC_READ,
 			FILE_SHARE_READ,
@@ -348,9 +256,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		bFtGot = true;
 	} while (false);
 
-	// Detect Ctrl or RButton early, PathIsUNC can take time
-	const bool bCtrlOrRbuttonPressed = ((GetKeyState(VK_CONTROL) < 0) || GetKeyState(VK_RBUTTON) < 0);
-
 	// rename
 	if (PathIsUNC(file1.c_str()) || PathIsUNC(file2.c_str()))
 	{
@@ -362,7 +267,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	}
 	else
 	{
-		switch(DoReplaceFileWithTransaction(file1, file2, fileback))
+		switch (DoReplaceFileWithTransaction(file1, file2, fileback))
 		{
 		case RTR_TRNAS_NA:
 			// transaction is not available
@@ -400,38 +305,38 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	wstring additionalMessage;
 	bool bShowRemoveConfirmMessage = !bAlwaysYes;
 
-	if(bCtrlOrRbuttonPressed)
-	{
-		bRemoveOld = true;
-		bShowRemoveConfirmMessage = true;
-	}
 	if (bRemoveOld)
 	{
 		do
 		{
 			bool isSame = false;
-			wstring olderfile = GetOlderFile(file1, file2, &isSame);
+			auto oldnew = GetOlderFile(file1, file2, &isSame);
 			if (isSame)
 			{
 				additionalMessage = I18N(L"File time is same.");
 				break;
 			}
-			if (olderfile.empty())
+			if (oldnew.olderFile.empty())
 			{
 				ErrorExit(I18N(L"Failed to find an older file."));
+			}
+			if (fnCondition)
+			{
+				if (!fnCondition(oldnew.olderFile, oldnew.newerFile))
+					break;
 			}
 			if (bShowRemoveConfirmMessage)
 			{
 				wstring sizeString;
 				LARGE_INTEGER liFileSize;
 				liFileSize.QuadPart = 0;
-				if (GetFileSizeFromPath(olderfile.c_str(), &liFileSize))
+				if (GetFileSizeFromPath(oldnew.olderFile.c_str(), &liFileSize))
 					sizeString = FormatSizeof(liFileSize.QuadPart);
 				else
 					sizeString = I18N(L"Unknown");
 
 				wstring msg = stdFormat(I18N(L"Do you want to remove '%s' of size '%s'?"),
-					olderfile.c_str(),
+					oldnew.olderFile.c_str(),
 					sizeString.c_str()
 				).c_str();
 
@@ -443,23 +348,213 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 					break;
 				}
 			}
-			int nRet = SHDeleteFileEx(olderfile.c_str());
+			int nRet = SHDeleteFileEx(oldnew.olderFile.c_str());
 			if (nRet != 0)
 			{
 				ErrorExit(GetLastErrorString(nRet));
 			}
-			additionalMessage = stdFormat(I18N(L"'%s' has been removed."), stdGetFileName(olderfile).c_str());
+			*pAdditionalMessage = stdFormat(I18N(L"'%s' has been removed."), stdGetFileName(oldnew.olderFile).c_str());
 		} while (false);
 	}
 
-	wstring balloontext = stdFormat(I18N(L"'%s' and '%s' have been swapped."), stdGetFileName(file1).c_str(), stdGetFileName(file2).c_str()) +
-		(additionalMessage.empty() ? L"" : L"\r\n" + additionalMessage);
+	return true;
+}
 
-	showballoonexe(
-		APP_NAME,
-		balloontext.c_str(),
-		stdGetModuleFileName().c_str(),
-		5000,
-		1);
+bool IsOldFileIsLessSize(wstring oldFile, wstring newFile)
+{
+	LARGE_INTEGER oldSize;
+	LARGE_INTEGER newSize;
+	if (!GetFileSizeFromPath(oldFile.c_str(), &oldSize) ||
+		!GetFileSizeFromPath(newFile.c_str(), &newSize))
+	{
+		ErrorExit(I18N(L"Failed to get file size"));
+	}
+	return oldSize.QuadPart < newSize.QuadPart;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	Ambiesoft::InitHighDPISupport();
+	wstring lang = GetLangFromCommandLine();
+	i18nInitLangmap(NULL, lang.c_str(), APPNAME);
+
+	CCommandLineParser parser(I18N(L"Swap filename of two files"));
+
+	bool bRemoveOld = false;
+	parser.AddOption(L"-removeold", 0, &bRemoveOld,
+		ArgEncodingFlags::ArgEncodingFlags_Default,
+		I18N(L"Remove an older file after swapping"));
+
+	wstring strRemoveOldIf;
+	parser.AddOption(L"-removeoldif", 1, &strRemoveOldIf,
+		ArgEncodingFlags::ArgEncodingFlags_Default,
+		I18N(L"Remove an older file after swapping if the condition is met. The condition is one of 'older_is_less_size', 'older_is_greater_size'"));
+
+	bool bAutoDetect = false;
+	parser.AddOption(L"-autodetect", 0, &bAutoDetect,
+		ArgEncodingFlags::ArgEncodingFlags_Default,
+		I18N(L"Detects a pair of files automatically, files with same name but extensions will be detected"));
+
+	bool bAutoDetectAll = false;
+	parser.AddOption(L"-autodetectall", 0, &bAutoDetectAll,
+		ArgEncodingFlags::ArgEncodingFlags_Default,
+		I18N(L"Detects all files automatically, files with same name but extensions will be detected"));
+
+	bool bAlwaysYes = false;
+	parser.AddOption(L"-alwaysyes", 0, &bAlwaysYes,
+		ArgEncodingFlags::ArgEncodingFlags_Default,
+		I18N(L"Answer 'yes' in all questions"));
+
+	bool bHelp = false;
+	parser.AddOptionRange({ L"-h",L"--help", L"/?" },
+		0,
+		&bHelp,
+		ArgEncodingFlags::ArgEncodingFlags_Default,
+		I18N(L"Show Help"));
+
+	bool bShowVersion = false;
+	parser.AddOptionRange({ L"-v",L"--version" },
+		0,
+		&bShowVersion,
+		ArgEncodingFlags::ArgEncodingFlags_Default,
+		I18N(L"Show Version"));
+
+	COption opMain(L"",
+		ArgCount::ArgCount_OneToInfinite,
+		ArgEncodingFlags_Default,
+		I18N(L"Input two files or directories if '-autodetect'"));
+	parser.AddOption(&opMain);
+
+	// lang is already defined
+	parser.AddOption(L"-lang",
+		1,
+		&lang,
+		ArgEncodingFlags_Default,
+		I18N(L"Language in 3-letter"));
+
+	parser.Parse();
+
+	if (parser.hadUnknownOption())
+	{
+		ErrorExit(I18N(L"Unknown Option:") + (L"\r\n" + parser.getUnknowOptionStrings()));
+	}
+	if (bShowVersion || bHelp)
+	{
+		MessageBox(NULL,
+			parser.getHelpMessage().c_str(),
+			stdFormat(L"%s v%s", APPNAME, GetVersionString(NULL, 3).c_str()).c_str(),
+			MB_ICONINFORMATION);
+		return 0;
+	}
+
+	struct F1F2
+	{
+		wstring file1;
+		wstring file2;
+	};
+	
+	vector<F1F2> filepairs;
+
+	if (bAutoDetect|| bAutoDetectAll)
+	{
+		// get dirs from command line
+		if (opMain.getValueCount() == 0)
+		{
+			ErrorExit(stdFormat(
+				I18N(L"Directory must be specified if '-autodetect'"),
+				opMain.getValueCount()));
+		}
+		wstring dir = opMain.getValue(0);
+		if (!stdDirectoryExists(dir))
+		{
+			ErrorExit(stdFormat(
+				I18N(L"'%s' is not a directory"),
+				dir.c_str()));
+		}
+		// detect files
+		vector<wstring> files = stdGetFiles(dir,
+			FILEITERATEMODE::SKIP_DOT_AND_DOTDOT,
+			GETFILESEMODE::FILE);
+
+		map<wstring, vector<wstring>> nameToFileNames;
+		for (auto&& file : files)
+		{
+			nameToFileNames[stdStringLower(stdGetFileNameWithoutExtension(file))].push_back(file);
+		}
+
+		for (auto&& kv : nameToFileNames)
+		{
+			if (nameToFileNames[kv.first].size() == 2)
+			{
+				filepairs.push_back(F1F2{ nameToFileNames[kv.first][0], nameToFileNames[kv.first][1] });
+				if (bAutoDetect)
+					break;  // detect only one pair
+			}
+		}
+	}
+	else
+	{
+		if (opMain.getValueCount() != 2)
+		{
+			ErrorExit(stdFormat(
+				I18N(L"The count of files provided by command line is %d. Two files must be provided."),
+				opMain.getValueCount()));
+		}
+		filepairs.push_back(F1F2{ opMain.getValue(0), opMain.getValue(1) });
+	}
+
+	// Detect Ctrl or RButton early, PathIsUNC can take time
+	const bool bCtrlOrRbuttonPressed = ((GetKeyState(VK_CONTROL) < 0) || GetKeyState(VK_RBUTTON) < 0);
+	if (bCtrlOrRbuttonPressed)
+	{
+		bRemoveOld = true;
+	}
+
+	function<bool(wstring oldFile, wstring newFile)> fnCondition = nullptr;
+	if (!strRemoveOldIf.empty())
+	{
+		if (strRemoveOldIf == L"older_is_less_size") {
+			fnCondition = IsOldFileIsLessSize;
+		} 
+		else if (strRemoveOldIf == L"older_is_greater_size") {
+			fnCondition = [](wstring oldFile, wstring newFile) {
+				return !IsOldFileIsLessSize(oldFile, newFile);
+			};
+		}
+		else
+		{
+			ErrorExit(I18N(L"Invalid condition:") + strRemoveOldIf);
+		}
+		bRemoveOld = true;
+	}
+
+	for (auto&& fp : filepairs)
+	{
+		wstring additionalMessage;
+		doWork(fp.file1, fp.file2,
+			bAlwaysYes, bRemoveOld,
+			&additionalMessage,
+			fnCondition);
+
+		wstring balloontext = stdFormat(
+			I18N(L"'%s' and '%s' have been swapped."),
+			stdGetFileName(fp.file1).c_str(),
+			stdGetFileName(fp.file2).c_str()) +
+			(additionalMessage.empty() ? L"" : L"\r\n" + additionalMessage);
+
+		showballoonexe(
+			APP_NAME,
+			balloontext.c_str(),
+			stdGetModuleFileName().c_str(),
+			5000,
+			1);
+	}
 	return 0;
 }
+
